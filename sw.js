5e8ea098f83dfeb61cfb4b814b3e08db240a05b5
Naldo <edinaldoandradeoficial@gmail.com>
2026-05-19 22:34:58 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,20 @@
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+const CACHE_NAME = 'naldoa-play-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/logo.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
