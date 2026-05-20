27972c6f58f8b9443e07e88f17a81e60ff3cbfe4
Naldo <edinaldoandradeoficial@gmail.com>
2026-05-19 22:24:58 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,3 @@
-// Service Worker básico para ativação do protocolo PWA da rádio
-self.addEventListener('install', function(event) {
-    self.skipWaiting();
-});
-
-self.addEventListener('activate', function(event) {
-    event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', function(event) {
-    // Permite que o streaming passe direto sem travar o buffer no cache
-    return;
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
 });
